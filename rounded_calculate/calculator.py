d653b616dbed9c9179c36417d0f9d4e30f03723d
Vytautas Ruzgaila <v.s.ruzgaila@gmail.com>
2023-02-14 16:11:28 +0200

    update

--- rounded_calculate/calculator.py
+++ rounded_calculate/calculator.py
@@ -1,25 +1,23 @@
 class Calculator:
-    """Define Calculator class.
-
-     All calculations made by class is rounded with 8 symbols precision.
-     Calculator memory is controled by Memory class.
-     
-     This class has methods to do perform:
-     I. Calculations: 
-       Addition, substraction, multiplication,
-       division, taking a (n) root of a number.
-     II. Memory related methods:
-       Reset memory, undo last operation stored
-       in Memory class, print calculator history.
-    """
+    # Define Calculator class.
+    # 
+    #  All calculations made by class is rounded with 8 symbols precision.
+    #  Calculator memory is controled by Memory class.
+    #  
+    #  This class has methods to do perform:
+    #  I. Calculations: 
+    #    Addition, substraction, multiplication,
+    #    division, taking a (n) root of a number.
+    #  II. Memory related methods:
+    #    Reset memory, undo last operation stored
+    #    in Memory class, print calculator history.
     def __init__(self):
         self.memory = Memory()
 
     def add(self, operand):
-        """Calculate addition by the given operand.
-
-        Calculations are based on the last Memory.history.total value.
-        """
+        # Calculate addition by the given operand.
+        # 
+        # Calculations are based on the last Memory.history.total value.
         try:
             base = self.memory.history[-1].total
             total = round(base + operand, 8)
@@ -29,10 +27,9 @@ class Calculator:
             print(f"Provided operand '{operand}' is not valid in {self.add.__name__} method")
 
     def substract(self, operand):
-        """Calculate substraction by the given operand.
-        
-        Calculations are based on the last Memory.history.total value.
-        """
+        # Calculate substraction by the given operand.
+        # 
+        # Calculations are based on the last Memory.history.total value.
         try:
             base = self.memory.history[-1].total
             total = round(base - operand, 8)
@@ -42,10 +39,9 @@ class Calculator:
             print(f"Provided operand '{operand}' is not valid in {self.substract.__name__} method")
 
     def multiply(self, operand):
-        """Calculate multiplication by the given operand.
-        
-        Calculations are based on the last Memory.history.total value.
-        """
+        # Calculate multiplication by the given operand.
+        # 
+        # Calculations are based on the last Memory.history.total value.
         try:    
             base = self.memory.history[-1].total
             total = round(base * operand, 8)
@@ -55,10 +51,9 @@ class Calculator:
             print(f"Provided operand '{operand}' is not valid in {self.multiply.__name__} method")
 
     def divide(self, operand):
-        """Calculate division by the given operand.
-        
-        Calculations are based on the last Memory.history.total value.
-        """
+        # Calculate division by the given operand.
+        # 
+        # Calculations are based on the last Memory.history.total value.
         try:
             base = self.memory.history[-1].total
             total = round(base / operand, 8)
@@ -68,45 +63,43 @@ class Calculator:
             print(f"Provided operand '{operand}' is not valid in {self.divide.__name__} method")
 
     def root(self, power):
-        """Calculate root of given power.
-        
-        Calculations are based on the last Memory.history.total value.
-        """
+        # Calculate root of given power.
+        # 
+        # Calculations are based on the last Memory.history.total value.
         try:
             base = self.memory.history[-1].total
             total = round(base ** (1/power), 8)
             self.memory.memory_add_item(base, "** 1 /", power, total)
             return total
         except:
-            print(f"Provided power of root '{power}' is not valid in {self.add.__name__} method")
+            print(f"Provided power of root '{power}' is not valid in {self.root.__name__} method")
 
 
     def reset_memory(self):
-        """Delete calculator memory object and initialize new Memory object."""
+        # Delete calculator memory object and initialize new Memory object.
         del self.memory
         self.memory = Memory()
 
     def undo(self):
-        """Remove last item in Memory.history list.
-        
-        Calls Memory class method.
-        """
+        # Remove last item in Memory.history list.
+        # 
+        # Calls Memory class method.
         self.memory.memory_remove_last()
 
     def print_history(self):
-        """Print history of all operations stored in Memory.history."""
+        # Print history of all operations stored in Memory.history.
         for item in self.memory.history:
             print(f"{item.base} {item.operation} {item.operand} = {+ item.total}")
 
 class Memory:
-    """Calculator module memory control."""
+    # Calculator module memory control.
 
     def __init__(self):
         self.history = []
         self.history.append(self.History_item(0, "Initial value", 0, 0))
 
     class History_item:
-        """History item construct to store at Memory.history."""
+        # History item construct to store at Memory.history.
 
         def __init__(self, base, operation, operand, total):
             self.base = base
@@ -115,10 +108,10 @@ class Memory:
             self.total = total
 
     def memory_add_item(self, base, operation, operand, total):
-        """Add new Memory.history entry using History_item construct."""
+        # Add new Memory.history entry using History_item construct.
         new_history_item = self.History_item(base, operation, operand, total)
         self.history.append(new_history_item)
 
     def memory_remove_last(self):
-        """Remove last item of Memory.history."""
+        # Remove last item of Memory.history.
         self.history.pop()
---
+++ tests/test_divide.py
@@ -0,0 +1,70 @@
+import os
+import sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+from rounded_calculate.calculator import Calculator
+from random import random
+
+calc = Calculator()
+
+def test_divide():
+    calc.reset_memory()
+    x2 = 0
+    for i in range(30):
+        x = random()*50
+        x = round(x,8)
+        x1 = calc.divide(x)
+        x2 /= x
+        assert x1 == round(x2,8)
+
+def test_divide_small():
+    calc.reset_memory()
+    x2 = 0
+    for i in range(30):
+        x = random()
+        x = round(x,8)
+        x1 = calc.divide(x)
+        x2 /= x
+        assert x1 == round(x2,8)
+
+def test_divide_large():
+    calc.reset_memory()
+    x2 = 0
+    for i in range(30):
+        try:
+            x = random()/1000000
+            x = round(x,8)
+            x1 = calc.divide(x)
+            x2 /= x
+            assert x1 == round(x2,8)
+        except ZeroDivisionError:
+            continue
+
+def test_divide_negative():
+    calc.reset_memory()
+    x2 = 0
+    for i in range(30):
+        x = -random()/50
+        x = round(x,8)
+        x1 = calc.divide(x)
+        x2 /= x
+        assert x1 == round(x2,8)
+
+def test_divide_inf():
+    calc.reset_memory()
+    x2 = 0 / float("inf")
+    x1 = calc.divide(float("inf"))
+    print(x1)
+    assert x1 == x2
+
+def test_divide_zero():
+    calc.reset_memory()
+    x1 = calc.divide(0)
+    assert x1 == None
+
+
+def test_divide_type():
+    calc.reset_memory()
+    x1 = calc.divide("a")
+    assert x1 == None
